b21410c9b8366b5f25110292c913f6a790b2cdff
s_hatta <hatta.shogo@gmail.com>
2024-11-02 12:40:26 +0900

    add: 共通レイアウトを追加する

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+</head>
+
+<body>
+
+  <head class="header">
+    <div class="header__inner">
+      <div class="header__logo">
+        FasionablyLate
+      </div>
+    </div>
+  </head>
+  @yield('content')
+</body>
+
+</html>
