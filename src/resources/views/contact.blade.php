8c949fd721d19dc81ed5c24028413820f092c7c6
s_hatta <hatta.shogo@gmail.com>
2024-11-04 00:18:19 +0900

    change: contact-table-row内のtdにclass名contact-table__itemを適用する

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -17,7 +17,7 @@
           <th class="contact-table__header">
             お名前 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="text" name="first_name" value="{{ old('first_name') }}" placeholder="例: 山田">
             <input type="text" name="last_name" value="{{ old('last_name') }}" placeholder="例: 太郎">
           </td>
@@ -28,7 +28,7 @@
           <th class="contact-table__header">
             性別 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="radio" name="gender" value="0" {{ old('gender', '0') === '0' ? 'checked' : '' }}>男性
             <input type="radio" name="gender" value="1" {{ old('gender') === '1' ? 'checked' : '' }}>女性
             <input type="radio" name="gender" value="2" {{ old('gender') === '2' ? 'checked' : '' }}>その他
@@ -40,7 +40,7 @@
           <th class="contact-table__header">
             メールアドレス ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="text" name="email" value="{{ old('email') }}" placeholder="例: test@example.com">
           </td>
         </tr>
@@ -50,7 +50,7 @@
           <th class="contact-table__header">
             電話番号 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="text" name='tell_1' value="{{ old('tell_1') }}" placeholder="080">
             -
             <input type="text" name='tell_2' value="{{ old('tell_2') }}" placeholder="1234">
@@ -64,7 +64,7 @@
           <th class="contact-table__header">
             住所 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="text" name="address" value="{{ old('address') }}" placeholder="例: 東京都渋谷区千駄々谷1-2-3">
           </td>
         </tr>
@@ -74,7 +74,7 @@
           <th class="contact-table__header">
             建物名
           </th>
-          <td>
+          <td class="contact-table__item">
             <input type="text" name="building" value="{{ old('building') }}" placeholder="例: 千駄ヶ谷マンション101">
           </td>
         </tr>
@@ -84,7 +84,7 @@
           <th class="contact-table__header">
             お問い合わせの種類 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <select name="category_id" required>
               <option value="" hidden>選択してください</option>
               @foreach ($categories as $category)
@@ -100,7 +100,7 @@
           <th class="contact-table__header">
             お問い合わせ内容 ※
           </th>
-          <td>
+          <td class="contact-table__item">
             <textarea name="detail" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
           </td>
         </tr>
