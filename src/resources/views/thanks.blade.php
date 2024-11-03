24ebd322a6e2a0f281ab1ec5181d2e780d992c81
s_hatta <hatta.shogo@gmail.com>
2024-11-03 17:40:56 +0900

    add: サンクスページを追加する

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>お問い合わせありがとうございました</title>
+</head>
+
+<body>
+  お問い合わせありがとうございました。
+  <form method="GET", action="/">
+    @csrf
+    <button type="submit">HOME</button>
+  </form>
+</body>
+
+</html>
