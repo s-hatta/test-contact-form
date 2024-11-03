ae27d77d1d08de8e317ec917f78d14b6ea545c2a
s_hatta <hatta.shogo@gmail.com>
2024-11-03 22:13:16 +0900

    change: contactsのemailのunique削除 なぜなら、同じ人が複数回問い合わせをすることはありえるため

--- src/database/migrations/2024_11_02_110635_create_contacts_table.php
+++ src/database/migrations/2024_11_02_110635_create_contacts_table.php
@@ -6,35 +6,35 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateContactsTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('contacts', function (Blueprint $table) {
-      $table->id();
-      $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-      $table->string('first_name');
-      $table->string('last_name');
-      $table->tinyInteger('gender');
-      $table->string('email')->unique();
-      $table->string('tell');
-      $table->string('address');
-      $table->string('building')->nullable();
-      $table->text('detail');
-      $table->timestamps();
-    });
-  }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->tinyInteger('gender');
+            $table->string('email');
+            $table->string('tell');
+            $table->string('address');
+            $table->string('building')->nullable();
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::dropIfExists('contacts');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
 }
