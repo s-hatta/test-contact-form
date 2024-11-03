08ee91428f7557429ce1298a435706312378a964
s_hatta <hatta.shogo@gmail.com>
2024-11-03 18:32:06 +0900

    fix: 電話番号のtypeとnameを修正する

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -48,7 +48,7 @@
         <tr class="confirm-table__row">
           <th class="confirm-table__header">電話番号</th>
           <td class="confirm-table__text">
-            <input type="tel" name="tel" value="{{ $contact['tell'] }}" readonly />
+            <input type="tell" name="tell" value="{{ $contact['tell'] }}" readonly />
           </td>
         </tr>
 
