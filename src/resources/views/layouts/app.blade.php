57f8fa1b6f0b93b33105534dcc4aaadcf74e31c1
s_hatta <hatta.shogo@gmail.com>
2024-11-03 23:28:12 +0900

    change: 共通ブレードの内容を変更する
    sanitize.cssとcommon.cssの読み込みを追加
    タイトルに対してスタイルシート適用

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
+  <link href='https://fonts.googleapis.com/css?family=Inika' rel='stylesheet'>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  @yield('css')
 </head>
 
 <body>
@@ -17,8 +21,12 @@
       </div>
     </div>
   </header>
-  @yield('name')
-  @yield('content')
+  <main class="main">
+    <div class="main__title">
+      @yield('name')
+    </div>
+    @yield('content')
+  </main>
 </body>
 
 </html>
