e78a8a7399f4e4e2904f09caaeb8e91b068c9135
s_hatta <hatta.shogo@gmail.com>
2024-11-03 18:43:02 +0900

    add: confirmからstoreしてthanksに遷移する処理を追加する

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Contact;
 use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
@@ -29,5 +30,22 @@ class ContactController extends Controller
         $contact += array('tell' => $request['tell_1'] . $request['tell_2'] . $request['tell_3']);
         $category = Category::find($contact['category_id']);
         return view('confirm', compact('contact', 'category'));
+    }
+
+    public function store(Request $request)
+    {
+        $contact = $request->only([
+            'first_name',
+            'last_name',
+            'gender',
+            'email',
+            'tell',
+            'address',
+            'building',
+            'category_id',
+            'detail',
+        ]);
+        Contact::create($contact);
+        return view('thanks');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ThanksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::post('/thanks', [ContactController::class, 'store']);
