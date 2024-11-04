4485752cc9dd466d3e3b2a5cda9c308d053f9b56
s_hatta <hatta.shogo@gmail.com>
2024-11-05 08:34:02 +0900

    change: ヘッダーにボタンを配置するためにauthを作り直ししてcssを追加

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -1,11 +1,42 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('header-extend')
-  <div class="header-nav">
-    @if (Auth::check())
-      <button class="header-nav__button-logout">logput</button>
-    @else
-      <button class="header-nav__button-login">login</button>
-    @endif
-  </div>
-@endsection
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <link href='https://fonts.googleapis.com/css?family=Inika' rel='stylesheet'>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/auth.css') }}" />
+  @yield('css')
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <div class="header__logo">
+        FasionablyLate
+      </div>
+      <div class="header-nav">
+        @if (Auth::check())
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button class="header-nav__button-logout">logput</button>
+          </form>
+        @else
+          <button class="header-nav__button-register">register</button>
+        @endif
+      </div>
+    </div>
+  </header>
+  <main class="main">
+    <div class="main__title">
+      @yield('name')
+    </div>
+    @yield('content')
+  </main>
+</body>
+
+</html>
