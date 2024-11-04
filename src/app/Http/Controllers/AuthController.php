7af4331b5c4f179f6846b73bb5603ca12022baac
s_hatta <hatta.shogo@gmail.com>
2024-11-05 08:49:34 +0900

    change: createは不要なため削除

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Contact;
 use App\Actions\Fortify\CreateNewUser;
@@ -14,17 +15,5 @@ class AuthController extends Controller
     {
         $contacts = Contact::Paginate(7)->onEachSide(2);
         return view('admin', compact('contacts'));
-    }
-
-    public function create(CreateNewUser $request)
-    {
-        User::create(
-            [
-                'name' => $request['user'],
-                'email' => $request['email'],
-                'password' => $request['password'],
-            ]
-        );
-        return view('login');
     }
 }
