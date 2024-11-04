f41842bd1a18b91cdace32690a6f8c6b4e9ec78a
s_hatta <hatta.shogo@gmail.com>
2024-11-05 02:59:19 +0900

    add: ユーザー追加用の処理を追加

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use App\Models\Contact;
+use App\Actions\Fortify\CreateNewUser;
+
 
 class AuthController extends Controller
 {
@@ -11,5 +14,17 @@ class AuthController extends Controller
     {
         $contacts = Contact::Paginate(7)->onEachSide(2);
         return view('admin', compact('contacts'));
+    }
+
+    public function create(CreateNewUser $request)
+    {
+        User::create(
+            [
+                'name' => $request['user'],
+                'email' => $request['email'],
+                'password' => $request['password'],
+            ]
+        );
+        return view('login');
     }
 }
