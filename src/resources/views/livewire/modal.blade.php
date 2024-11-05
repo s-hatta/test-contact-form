550549057e9647dcb1a75740b8176b0a4b32cc93
s_hatta <hatta.shogo@gmail.com>
2024-11-06 06:51:29 +0900

    add: 各項目を表示できるようにした

--- src/resources/views/livewire/modal.blade.php
+++ src/resources/views/livewire/modal.blade.php
@@ -9,7 +9,48 @@
           <table>
             <tr>
               <th>お名前</th>
-              <td>{{ $contact['id'] }}
+              <td>{{ $contact['last_name'] }}　{{ $contact['first_name'] }}</td>
+            </tr>
+            <tr>
+              <th>性別</th>
+              <td>
+                @switch($contact['gender'])
+                  @case(0)
+                    男性
+                  @break
+
+                  @case(1)
+                    女性
+                  @break
+
+                  @default
+                    その他
+                @endswitch
+              </td>
+            </tr>
+            <tr>
+              <th>メールアドレス</th>
+              <td>{{ $contact['email'] }}</td>
+            </tr>
+            <tr>
+              <th>電話番号</th>
+              <td>{{ $contact['tell'] }}</td>
+            </tr>
+            <tr>
+              <th>住所</th>
+              <td>{{ $contact['address'] }}</td>
+            </tr>
+            <tr>
+              <th>建物名</th>
+              <td>{{ $contact['building'] }}</td>
+            </tr>
+            <tr>
+              <th>お問い合わせの種類</th>
+              <td>{{ $contact['category']['content'] }}</td>
+            </tr>
+            <tr>
+              <th>お問い合わせ内容</th>
+              <td>{{ $contact['detail'] }}</td>
             </tr>
           </table>
         </div>
