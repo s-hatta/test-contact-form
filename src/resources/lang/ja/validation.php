dd71296088cff66d13d338b877dd030d18eef1a0
s_hatta <hatta.shogo@gmail.com>
2024-11-05 09:46:53 +0900

    change: 認証画面のメッセージを仕様通りになるよう変更する

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -133,6 +133,16 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
+        'name' => [
+            'required' => 'お名前を入力してください',
+        ],
+        'email' => [
+            'required' => 'メールアドレスを入力してください',
+            'email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+        ],
+        'password' => [
+            'required' => 'パスワードを入力してください',
+        ],
     ],
 
     /*
