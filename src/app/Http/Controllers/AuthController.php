0415fc2458e7a9047e05486c48d295ac665a5530
s_hatta <hatta.shogo@gmail.com>
2024-11-05 15:36:21 +0900

    change: adminページにカテゴリを渡すよう変更

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,13 +7,14 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Contact;
 use App\Actions\Fortify\CreateNewUser;
-
+use App\Models\Category;
 
 class AuthController extends Controller
 {
     public function index()
     {
         $contacts = Contact::Paginate(7)->onEachSide(2);
-        return view('admin', compact('contacts'));
+        $categories = Category::get()->all();
+        return view('admin', compact('contacts', 'categories'));
     }
 }
