4ac3c1b23f40e773d5f009a372735d65d5b277cc
s_hatta <hatta.shogo@gmail.com>
2024-11-05 17:07:57 +0900

    add: thanksページにスタイルシートを適用

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -6,14 +6,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>お問い合わせありがとうございました</title>
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
 </head>
 
 <body>
-  お問い合わせありがとうございました。
-  <form method="GET", action="/">
-    @csrf
-    <button type="submit">HOME</button>
-  </form>
+  <div class="content-thanks">
+    <div class="content-thanks__item">
+      <div class="content-thanks__item-text">
+        お問い合わせありがとうございました。
+      </div>
+      <form method="GET", action="/">
+        @csrf
+        <button class="content-thanks__item-submit" type="submit">HOME</button>
+      </form>
+    </div>
+  </div>
+  <label class="background-text">Thank you</label>
 </body>
 
 </html>
