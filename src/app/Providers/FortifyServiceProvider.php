c0b031fb94d596a9d31fd67881c742b33dbb1670
s_hatta <hatta.shogo@gmail.com>
2024-11-06 17:23:05 +0900

    fix: 登録後にログイン画面に遷移できていなかったのを修正する
    既存のRegisteredUserControllerをカスタマイズしたRegisterControllerにて実装

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -13,6 +13,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\Contracts\LogoutResponse;
+use App\Http\Controllers\RegisterController;
+use Laravel\Fortify\Http\Controllers\RegisterController as ControllersRegisterController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -21,12 +24,10 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
-            public function toResponse($request)
-            {
-                return redirect('/login');
-            }
-        });
+        $this->app->singleton(
+            RegisteredUserController::class,
+            RegisterController::class
+        );
     }
 
     /**
