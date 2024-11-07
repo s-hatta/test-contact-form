f801a74e28669218cbca28d37c40970796d2e0bd
s_hatta <hatta.shogo@gmail.com>
2024-11-07 20:35:22 +0900

    add: 各項目にplaceholderを追加

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="form__group-content">
           <div class="form__input--text">
-            <input type="text" name="email" value="{{ old('email') }}" />
+            <input type="text" name="email" value="{{ old('email') }}" placeholder="例: test@example.com" />
           </div>
           <div class="form__error">
             @error('email')
@@ -37,7 +37,7 @@
         </div>
         <div class="form__group-content">
           <div class="form__input--text">
-            <input type="password" name="password" />
+            <input type="password" name="password" placeholder="例: coachtech06" />
           </div>
           <div class="form__error">
             @error('password')
